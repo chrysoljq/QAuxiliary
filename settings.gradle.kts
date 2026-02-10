b172471c045b35cb4c837a275b6f8e952f08c30f
chrysoljq <study@iqach.top>
2026-02-10 22:26:19 +0800

    test build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -25,7 +25,7 @@ dependencyResolutionManagement {
             content {
                 includeGroup("com.github.livefront.sealed-enum")
                 includeGroup("com.github.MatteoBattilana")
-                // includeGroup("com.github.plattysoft")
+                includeGroup("com.github.plattysoft")
             }
         }
         maven("https://api.xposed.info/") {
@@ -34,16 +34,6 @@ dependencyResolutionManagement {
             }
         }
         mavenCentral()
-        maven("https://maven.tmpfs.dev/repository/maven-public/") {
-            // I have no idea why sometimes jitpack.io is not working for
-            // "com.github.plattysoft:Leonids:1746429"
-            // So I added this repo as a backup.
-            // I have encountered this wired issue twice in 2024.
-            // The jitpack.io says "Not found" or "File not found. Build ok".
-            content {
-                includeGroup("com.github.plattysoft")
-            }
-        }
     }
 }
 
